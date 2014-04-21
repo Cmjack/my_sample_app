8e4e6e545c14695580a36c07be67a15859156162
Your Name <you@example.com>
2014-04-21 17:45:41 +0800

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 MySampleApp::Application.routes.draw do
+  get "users/new"
   root 'static_pages#home'
   
   match '/help', to: 'static_pages#help', via: 'get'
   match '/about', to: 'static_pages#about', via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
   match '/root', to: 'static_pages#home', via: 'get'
+  match '/signup',  to: 'users#new',            via: 'get'
 
   # get about_ptah =>"static_pages/about"
   # The priority is based upon order of creation: first created -> highest priority.
